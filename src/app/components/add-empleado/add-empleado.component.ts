8fed86a6efceb2b18098fc1bf3f663a3dbe8cbeb
YheremiRamos <yheremiramosquispe@gmail.com>
2024-04-19 09:02:55 -0500

    Front

---
+++ src/app/components/add-empleado/add-empleado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-empleado',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-empleado.component.html',
+  styleUrl: './add-empleado.component.css'
+})
+export class AddEmpleadoComponent {
+
+}
---
+++ src/app/models/empleado.model.ts
@@ -0,0 +1,18 @@
+import { Pais } from "./pais.model";
+
+export class Empleado {
+
+    idEmpleado ?:number;
+    nombres?:string;
+    apellidos?:string;
+    fechaNacimiento ?:Date;
+    estado?:number;
+    fechaRegistro?:Date;
+    fechaActualizacion?:Date;
+    pais?:Pais;
+
+
+    
+
+
+}
